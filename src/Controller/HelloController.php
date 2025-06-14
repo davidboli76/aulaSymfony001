c05b9409564ad2603007aba83d709ca4c4a2b905
David Barreto <davidbarretobk@gmail.com>
2025-06-13 22:30:03 -0300

    coringas

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class HelloController
 {
@@ -13,45 +14,53 @@ class HelloController
         return new Response('Esta é a página principal!');
     }
 
-    #[Route('/pagina01')]
-    public function pagina01(): Response
+    #[Route('/animal/{slug}')]
+    public function animal(string $slug = null): Response
     {
-        return new Response('Esta é a página principal!');
+        $newSlug = str_replace('-', ' ', $slug);
+        $newSlug = u($newSlug)->title(true);
+        return new Response('O animal escolhido foi ' . $newSlug);
     }
 
-    #[Route('/pagina02')]
-    public function pagina02(): Response
-    {
-        return new Response('Esta é a segunda página!');
-    }
+    // #[Route('/pagina01')]
+    // public function pagina01(): Response
+    // {
+    //     return new Response('Esta é a página principal!');
+    // }
 
-    #[Route('/pagina03')]
-    public function pagina03(): Response
-    {
-        return new Response('Esta é a terceira página!');
-    }
+    // #[Route('/pagina02')]
+    // public function pagina02(): Response
+    // {
+    //     return new Response('Esta é a segunda página!');
+    // }
 
-    #[Route('/pagina04')]
-    public function pagina04(): Response
-    {
-        return new Response('Esta é a quarta página!');
-    }
+    // #[Route('/pagina03')]
+    // public function pagina03(): Response
+    // {
+    //     return new Response('Esta é a terceira página!');
+    // }
 
-    #[Route('/pagina05')]
-    public function pagina05(): Response
-    {
-        return new Response('Esta é a quinta página!');
-    }
+    // #[Route('/pagina04')]
+    // public function pagina04(): Response
+    // {
+    //     return new Response('Esta é a quarta página!');
+    // }
 
-    #[Route('/pagina06')]
-    public function pagina06(): Response
-    {
-        return new Response('Esta é a sexta página!');
-    }
+    // #[Route('/pagina05')]
+    // public function pagina05(): Response
+    // {
+    //     return new Response('Esta é a quinta página!');
+    // }
 
-    #[Route('/pagina07')]
-    public function pagina07(): Response
-    {
-        return new Response('Esta é a sétima página!');
-    }
+    // #[Route('/pagina06')]
+    // public function pagina06(): Response
+    // {
+    //     return new Response('Esta é a sexta página!');
+    // }
+
+    // #[Route('/pagina07')]
+    // public function pagina07(): Response
+    // {
+    //     return new Response('Esta é a sétima página!');
+    // }
 }
