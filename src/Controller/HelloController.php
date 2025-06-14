5fb57b8180cd99e991a67d009bcc82f78660dba7
David Barreto <davidbarretobk@gmail.com>
2025-06-14 19:40:34 -0300

    Usando as {chaves} do twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,38 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class HelloController
+class HelloController extends AbstractController
 {
     #[Route('/')]
+    // public function index(): Response
+    // {
+    //     return new Response('Esta é a página principal!');
+    // }
+
     public function index(): Response
     {
-        return new Response('Esta é a página principal!');
+        $users = [
+            ['nome' => 'Jocasta Nu', 'telefone' => '(81) 9.9999.5568'],
+            ['nome' => 'Obi wan Kenobi', 'telefone' => '(81) 9.5844.5522'],
+            ['nome' => 'Darth Plaguels', 'telefone' => '(81) 9.8632.3232'],
+            ['nome' => 'Lea Organa', 'telefone' => '(81) 9.9466.5888'],
+            ['nome' => 'Jar-jar Binks', 'telefone' => '(81) 9.6457.5200'],
+            ['nome' => 'Jango Feet', 'telefone' => '(81) 9.9091.0308']
+        ];
+
+        // dump($users);
+        // exit;
+
+        return $this->render('hello/homepage.html.twig', [
+            'title' => 'Meu manicômio',
+            'boasvindas' => 'Welcome to meu mundo, prego!',
+            'users' => $users,
+        ]);
     }
 
     #[Route('/animal/{slug}')]
